742dc4d918cdc7ae2b8076b0f52d78e6db171ba6
rishadtharayil <46060826+rishadtharayil@users.noreply.github.com>
2026-02-02 20:10:36 +0530

    Remove dotenv loading from manage.py
    Removed dotenv import and loading from manage.py

--- backend/manage.py
+++ backend/manage.py
@@ -6,8 +6,6 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    import dotenv
-    dotenv.load_dotenv()
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
     try:
         from django.core.management import execute_from_command_line
